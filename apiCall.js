3fc6d3ee46257040d5d243b950dbeaaa357e456a
sebitacasa <seba.-14@hotmail.com>
2025-06-12 20:32:21 +1000

    changes

--- apiCall.js
+++ apiCall.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 require("dotenv").config();
-const { API_KEY } = process.env;
+const API_KEY = process.env;
 
 
 
